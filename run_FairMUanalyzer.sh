4c1532cf764c0b255e0f10c8496d9324dcd3c3e6
CedricZhang9516 <122109921+cezhangliv@users.noreply.github.com>
2025-08-29 13:50:59 +0100

    Update run_FairMUanalyzer.sh

--- run_FairMUanalyzer.sh
+++ run_FairMUanalyzer.sh
@@ -53,4 +53,4 @@
 ### 29 Aug 25 GA discussion test, approved plots
 
 ./run_FairMUanalyzer root/single_muon_interaction_1_CDbugfix11July25_run8_muedaq04-1750227094-1750228937_MF1.root \
-	result/FairMUanalyzer_29Aug25_single_muon_interaction_1_CDbugfix11July25_run8_muedaq04-1750227094-1750228937_MF1 10000 1
+	result/FairMUanalyzer_29Aug25_single_muon_interaction_1_CDbugfix11July25_run8_muedaq04-1750227094-1750228937_MF1 100000 1
