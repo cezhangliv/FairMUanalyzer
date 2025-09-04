b2b4552daea0b9e4e98c12b7605cb1ce6a43bcdc
CedricZhang9516 <122109921+cezhangliv@users.noreply.github.com>
2025-09-04 10:12:01 +0100

    debug

--- src/FairMUanalyzer.cpp
+++ src/FairMUanalyzer.cpp
@@ -280,15 +280,15 @@ double FairMUanalyzer::acoplanarity(const TVector3 in, const TVector3 out1, cons
 }
 
 
-double FairMUanalyzer::CalculateXtgt(MUonERecoOutputTrackAnalysis track, double zTgt){
-    return track.xSlope()*(zTgt-track.z0())+track.x0();
+double FairMUanalyzer::CalculateXtgt(MUonERecoOutputTrackAnalysis* track, double zTgt){
+    return track->xSlope()*(zTgt-track->z0())+track->x0();
 }
 
-double FairMUanalyzer::CalculateYtgt(MUonERecoOutputTrackAnalysis track, double zTgt){
-    return track.ySlope()*(zTgt-track.z0())+track.y0();
+double FairMUanalyzer::CalculateYtgt(MUonERecoOutputTrackAnalysis* track, double zTgt){
+    return track->ySlope()*(zTgt-track->z0())+track->y0();
 }
 
-double FairMUanalyzer::CalculateRtgt(MUonERecoOutputTrackAnalysis track, double zTgt){
+double FairMUanalyzer::CalculateRtgt(MUonERecoOutputTrackAnalysis* track, double zTgt){
 
     return sqrt(CalculateXtgt( track,  zTgt)*CalculateXtgt( track,  zTgt)+CalculateYtgt( track,  zTgt)*CalculateYtgt( track,  zTgt));
 
